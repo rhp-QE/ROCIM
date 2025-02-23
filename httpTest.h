60642f546a8a3001244c837b0525e357f35ccb52
root <root@WINDOWS-ROC>
2025-02-24 00:56:25 +0800

    http json 格式数据测试

--- httpTest.h
+++ httpTest.h
@@ -1,6 +1,7 @@
 
 #include <boost/json.hpp>
 #include <base/HttpClient.h>
+#include <boost/json/array.hpp>
 #include <boost/json/object.hpp>
 #include <boost/json/parse.hpp>
 #include <boost/json/serialize.hpp>
@@ -10,9 +11,14 @@
 int testHttp() {
     using namespace::roc::net;
 
+    boost::json::object json_sub1;
+    json_sub1["name"] = "rxy";
+    json_sub1["age"] = 12;
+
     boost::json::object json_sub;
     json_sub["name"] = "rxy";
     json_sub["age"] = 12;
+    json_sub["lick"] = boost::json::array{90,100,100, 89, json_sub1};
 
     boost::json::object json;
     json["name"] = "rhp";
